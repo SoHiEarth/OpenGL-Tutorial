2e98047ca5e807046120793b5f6d010070d814bb
SoHiEarth <126925211+SoHiEarth@users.noreply.github.com>
2025-04-04 22:11:46 +0900

    feat: Complete until "Our own shader class" of "Shaders"

--- src/main.cc
+++ src/main.cc
@@ -4,10 +4,11 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 float vertices[] = {
-  -0.5f, -0.5f, 0.0f,
-  0.5f, -0.5f, 0.0f,
-  0.0f,  0.5f, 0.0f
+  -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+  0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+  0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 };
 
 std::string ReadBinFile(const char* path) {
@@ -97,8 +98,10 @@ int main(int argc, char** argv) {
   glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 
@@ -109,6 +112,10 @@ int main(int argc, char** argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
+    float timeValue = glfwGetTime();
+    float colorValue = (sin(timeValue) / 2.0f) + 0.5f;
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "timeOffset");
+    glUniform4f(vertexColorLocation, 0.0f, colorValue, 0.0f, 1.0f);
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
